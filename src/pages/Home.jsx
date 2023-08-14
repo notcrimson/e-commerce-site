379ae8a250a6ca0af0bf64dbf956d2c1c50ef729
Artem Sedov <artden1mc@gmail.com>
2023-08-14 22:19:31 +0300

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from "../components";
+
+const Home = () => {
+  return (
+    <div className="bg-black">
+      Home
+      <div className="w-full">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
