061867cd35bdd6078bc76e82fa657340799bfa75
Artem Sedov <artden1mc@gmail.com>
2023-11-07 00:56:09 +0300

    types.json & show categories onhover for each type

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import { useGetAllCategoriesQuery } from "../redux/apiSlice";
+import { types } from "../constants/types.json";
 
 const Categories = () => {
   const { data } = useGetAllCategoriesQuery();
   console.log(data);
+  // const types = {
+  //   technology: {
+  //     categories: ["smartphones", "laptops"],
+  //   },
+  //   skincare: {
+  //     categories: ["fragrances", "skincare"],
+  //   },
+  //   home: {
+  //     categories: ["home-decoration", "furniture", "lighting"],
+  //   },
+  //   clothing: {
+  //     categories: ["tops", "womens-dresses", "womens-shoes", "mens-shirts", "mens-shoes"],
+  //   },
+  //   accessories: {
+  //     categories: [
+  //       "mens-watches",
+  //       "womens-watches",
+  //       "womens-bags",
+  //       "womens-jewellery",
+  //       "sunglasses",
+  //     ],
+  //   },
+  //   other: {
+  //     categories: ["groceries", "automotive", "motorcycle"],
+  //   },
+  // };
+  console.log(types);
 
   return (
     <div className="relative mb-10">
@@ -12,7 +40,19 @@ const Categories = () => {
           <h1 className="section-heading pt-[8px] pb-[20px] ">Categories</h1>
         </div>
         <div className="category__section px-20 grid grid-cols-3 grid-flow-row-dense gap-2 ">
-          <div name="Technology" className="group category relative ">
+          {types.map((type) => (
+            <div key={type.name} className="group category relative">
+              <div className="absolute z-10 text-center inset-x-0 m-auto top-[50%] translate-y-[-50%] hidden group-hover:block text-xl font-extrabold">
+                {type.categories.map((category) => (
+                  <h2 key={category} className="text-white">
+                    {category}
+                  </h2>
+                ))}
+              </div>
+              <h1 className="group-hover:blur-sm capitalize">{type.name}</h1>
+            </div>
+          ))}
+          {/* <div name="Technology" className="group category relative ">
             <div className="group-hover:catergory__description-b "></div>
             <div className="absolute z-10 text-center inset-x-0 m-auto top-[50%] translate-y-[-50%] hidden group-hover:block text-xl font-extrabold">
               <h2 className="text-white ">Something</h2>
@@ -35,7 +75,7 @@ const Categories = () => {
           </div>
           <div name="Other" className="group category">
             <h1 className="group-hover:blur-sm">Other</h1>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
